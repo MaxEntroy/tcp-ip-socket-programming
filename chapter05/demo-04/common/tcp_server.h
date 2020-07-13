658aaca69639fd3831f13dafe79eb9d15d66a175
MaxEntroy <likang@tju.edu.cn>
2020-07-13 20:54:10 +0800

    chapter05: Start to implement TcpServer.

--- chapter05/demo-04/common/tcp_server.h
+++ chapter05/demo-04/common/tcp_server.h
@@ -1,12 +1,27 @@
 #ifndef UTILS_TCP_SERVER_H_
 #define UTILS_TCP_SERVER_H_
 
+#include <unistd.h>
+
+#include <memory>
+
 namespace utils {
 
+struct SocketDeleter {
+  void operator()(int* p_sfd) {
+    close(*p_sfd);
+  }
+};
+
 class TcpServer {
+ public:
+  explicit TcpServer(int port) {}
 
+  TcpServer(const TcpServer&) = delete;
+  TcpServer& operator=(const TcpServer&) = delete;
 
-
+ private:
+  std::unique_ptr<int, SocketDeleter> sfd_ptr_;
 }; // TcpServer
 
 } // namespace utils
