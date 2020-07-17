3644107a89a456e9e95eaf0b0cb617104684f6ae
MaxEntroy <likang@tju.edu.cn>
2020-07-17 19:26:32 +0800

    chapter05: TcpClient is done.

--- chapter05/demo-04/common/tcp_client.h
+++ chapter05/demo-04/common/tcp_client.h
@@ -1,9 +1,30 @@
 #ifndef UTILS_TCP_CLIENT_H_
 #define UTILS_TCP_CLIENT_H_
 
+#include <unistd.h>
+#include <arpa/inet.h>
+
 namespace utils {
 
-class TcpClient {};
+class TcpClient {
+ public:
+  TcpClient();
+
+  ~TcpClient() { close(clnt_sfd_); }
+  TcpClient(const TcpClient&) = delete;
+  TcpClient& operator=(const TcpClient&) = delete;
+
+ public:
+  void Init(const char* ip, int port);
+  void Run();
+
+ private:
+  virtual void HandleIoEvent() = 0;
+
+ private:
+  int clnt_sfd_;
+  struct sockaddr_in serv_addr_;
+}; // class TcpClient
 
 } // namespace utils
 
