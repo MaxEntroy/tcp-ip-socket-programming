e45dd85a1431fde2f07d4a7d9b89296dfd46b4e4
MaxEntroy <likang@tju.edu.cn>
2020-09-27 20:22:23 +0800

    chapter12: Add demo-01.

--- chapter12/README.md
+++ chapter12/README.md
@@ -6,7 +6,6 @@
 2. 理解select函数并实现服务器端
 3. 基于windows的实现
 
-
 q:Blocking?
 >Blocking. You’ve heard about it—now what the heck is it? In a nutshell, “block” is techie jargon for “sleep”.
 当然，这里其实在os进程状态有过讨论，这里再给出一个简短的说明
@@ -32,3 +31,31 @@ q:io复用的优点？
 we’re going to ask the operating system to do all the dirty work for us, and just let us know
 when some data is ready to read on which sockets. In the meantime, our process can go to sleep, saving
 system resources
+
+### 实践
+
+- demo-01
+
+select server.
+
+q:io event loop，也是循环处理，不处理完一个，无法处理下一个，这个效率高在哪里？
+>虽然不处理玩一个无法处理下一个，但是此时cpu没有阻塞，而是在running.所以，cpu资源没有浪费。
+这是io复用的优点，就是io ready时才返回，此时cpu处理即可，cpu并没有阻塞在io上，资源没有浪费。
+其次，由于cpu处理很快，所以cpu的处理loop没有浪费处理机资源，再快也就是这样了
+
+q:select一些坑？
+>select对于fd_set以及timeout是传出参数，所以要保存副本。每次传入时进行更新
+
+q:目前的实现是否有问题？
+>HandleIoEvent实现的有问题。
+多进程服务器，由于子进程进行fd的阻塞read，父进程没有被阻塞得以处理connection.
+但是，io 复用的作用是监听所有fd io event.但是，父类规定的HandleIoEvent是处理完成所有数据。
+当前程序没有bug，是因为一次发送数据发送完成了。我们在HandleIoEvent当中调用了io_aux当中的io_read_n函数。
+保证tcp的多次传输，可以确保获取完整的数据。但是select只监听了一次，比如传递了部分数据。
+HandleIoEvent第一次读不阻塞，但是没有获取数据完成，会继续调用read，阻塞住当前进程。导致io 复用失败。
+>
+如何解决，如果还要保持当前的结构。只能是HandleIoEvent保持状态，不然没有办法。只有获取的数据读取完毕，才能继续进行
+leo给了建议，需要管理fd的buffer
+>
+暂时没有想到特别好的方式，先没有解决这个问题。先向下走，到epoll看看有没有好的办法
+
--- chapter12/demo-01/common/tcp_server.cc
+++ chapter12/demo-01/common/tcp_server.cc
@@ -7,6 +7,9 @@
 #include <sys/socket.h>
 #include <sys/select.h>
 #include <sys/wait.h>
+
+#include <algorithm>
+
 #include "err.h"
 
 namespace utils {
@@ -54,36 +57,49 @@ void TcpServer::EventLoop() {
   fd_set reads, cpy_reads;
   FD_ZERO(&reads);
   FD_SET(listen_sfd_, &reads);
-  int max_fd = listen_sfd_ + 1;
+
+  int max_sfd = listen_sfd_;
+  struct timeval timeout;
 
   while(1) {
     printf("Tcp server[localhost:%d] waiting...\n", ntohs(serv_addr_.sin_port));
 
     cpy_reads = reads;
-
-
-    // accept
-    int clnt_sfd = accept(listen_sfd_, (struct sockaddr*) &clnt_addr, &clnt_addr_len);
-    if (clnt_sfd == -1) {
+    timeout.tv_sec = 5;
+    timeout.tv_usec = 0;
+    int ret = select(max_sfd + 1, &cpy_reads, NULL, NULL, &timeout);
+    if (ret == -1) {
+      perr_handling("select", "error");
+    } else if (ret == 0) {
       continue;
-    }
-    printf("[%s:%d] connected.\n", inet_ntoa(clnt_addr.sin_addr), ntohs(clnt_addr.sin_port));
-
-    pid_t pid = fork();
-    if (pid == -1) {
-      close(clnt_sfd);
-      continue;
-    }
-
-    if (!pid) {
-      // IO
-      HandleIoEvent(clnt_sfd);
-
-      close(clnt_sfd);
-      printf("[%s:%d] disconnected.\n", inet_ntoa(clnt_addr.sin_addr), ntohs(clnt_addr.sin_port));
-      return;
     } else {
-      close(clnt_sfd);
+      for (int sfd = 0; sfd <= max_sfd; ++sfd) {
+        if (!FD_ISSET(sfd, &cpy_reads)) {
+          continue;
+        }
+        // if: listen_sfd io event
+        // else: other sfd io event
+        if (sfd == listen_sfd_) {
+          int clnt_sfd = accept(listen_sfd_, (struct sockaddr*) &clnt_addr, &clnt_addr_len);
+          if (clnt_sfd == -1) {
+            perr_handling("accept", "error");
+          }
+
+          //Add clnt_sfd to select
+          FD_SET(clnt_sfd, &reads);
+          max_sfd = std::max(clnt_sfd, max_sfd);
+
+          printf("[%s:%d] connected.\n", inet_ntoa(clnt_addr.sin_addr), ntohs(clnt_addr.sin_port));
+        } else {
+          HandleIoEvent(sfd);
+          close(sfd);
+
+          // Remove clnt_sfd to select
+          FD_CLR(sfd, &reads);
+          max_sfd = (max_sfd == sfd)?(max_sfd - 1):max_sfd;
+          printf("[%s:%d] disconnected.\n", inet_ntoa(clnt_addr.sin_addr), ntohs(clnt_addr.sin_port));
+        }
+      }
     }
   }
 }
